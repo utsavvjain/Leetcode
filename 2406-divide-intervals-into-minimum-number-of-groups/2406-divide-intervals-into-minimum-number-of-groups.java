8b157565a562301e014c7e26b5789d9ff619cc7f
utsav jain <74541660+utsavvjain@users.noreply.github.com>
2022-09-11 14:59:05 +0530

    Time: 178 ms (20.00%), Space: 163.8 MB (20.00%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -1,11 +1,16 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int n=intervals.length;
+        int []graph=new int[1000002];
         for(int []interval : intervals){
-            if(!pq.isEmpty() && pq.peek()<interval[0]) pq.poll();
-            pq.offer(interval[1]);
+            graph[interval[0]]++;
+            graph[interval[1]+1]--;
         }
-        return pq.size();
+        int mini=graph[0];
+        for(int i=1;i<graph.length;i++){
+            graph[i]+=graph[i-1];
+            mini=Math.max(mini,graph[i]);
+        }
+        return mini;
     }
 }
