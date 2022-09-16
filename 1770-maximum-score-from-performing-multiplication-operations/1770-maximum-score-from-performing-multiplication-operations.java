f97223137c4eee3400037981c9b7b261e9799792
utsav jain <74541660+utsavvjain@users.noreply.github.com>
2022-09-16 22:49:01 +0530

    Time: 200 ms (55.55%), Space: 101.9 MB (75.91%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,16 +1,16 @@
 class Solution {
+    public int maximumScore(int[] nums, int[] multipliers,int lIndex,int mIndex,int dp[][]) {
+        int n=nums.length;
+        if(mIndex==multipliers.length) return 0;
+        if(lIndex>mIndex) return 0;
+        if(dp[lIndex][mIndex]!=Integer.MIN_VALUE) return dp[lIndex][mIndex];
+        int left=multipliers[mIndex]*nums[lIndex] + maximumScore(nums,multipliers,lIndex+1,mIndex+1,dp);
+        int right=multipliers[mIndex]*nums[n-1-(mIndex-lIndex)] + maximumScore(nums,multipliers,lIndex,mIndex+1,dp); //to calculate right index
+        return dp[lIndex][mIndex]=Math.max(left,right);
+    }
     public int maximumScore(int[] nums, int[] multipliers) {
-        int dp[]=new int[multipliers.length+1];
-        int n=nums.length;        
-        for(int mIndex=multipliers.length-1;mIndex>=0;mIndex--){
-            int []nw=dp.clone();
-            for(int lIndex=mIndex;lIndex>=0;lIndex--){ //left at max can be mIndex
-                int left=multipliers[mIndex]*nums[lIndex] + nw[lIndex+1];
-                int right=multipliers[mIndex]*nums[n-1-(mIndex-lIndex)] + nw[lIndex];
-                dp[lIndex]=Math.max(left,right);
-            }
-        }
-        return dp[0];
-    
+        int dp[][]=new int[multipliers.length][multipliers.length];
+        for(int i=0;i<dp.length;i++) for(int j=0;j<dp[i].length;j++) dp[i][j]=Integer.MIN_VALUE;
+        return maximumScore(nums,multipliers,0,0,dp);
     }
 }
