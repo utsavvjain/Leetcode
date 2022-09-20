5eaf1d3c35dc30d59e1242d111f3cbadc0151570
utsav jain <74541660+utsavvjain@users.noreply.github.com>
2022-09-20 14:12:51 +0530

    Time: 74 ms (65.22%), Space: 79.5 MB (21.56%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,23 +1,15 @@
 class Solution {    
-    int sln=0;
-    public int lcs(int[] nums1, int[] nums2,int i,int j,int[][] dp) {
-        if(i==nums1.length || j==nums2.length) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
-        if(nums1[i]==nums2[j]){
-            ans=1+lcs(nums1,nums2,i+1,j+1,dp);
-            sln=Math.max(sln,ans);            
-        }
-        lcs(nums1,nums2,i+1,j,dp);
-        lcs(nums1,nums2,i,j+1,dp);
-        return dp[i][j]=ans;
-    }
     public int findLength(int[] nums1, int[] nums2) {
-        int dp[][]=new int[nums1.length][nums2.length];
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++) dp[i][j]=-1;
+        int dp[][]=new int[nums1.length+1][nums2.length+1];
+        int ans=0;
+        for(int i=nums1.length-1;i>=0;i--){
+            for(int j=nums2.length-1;j>=0;j--){
+                if(nums1[i]==nums2[j]){
+                    dp[i][j]=Math.max(dp[i][j],1+dp[i+1][j+1]);
+                    ans=Math.max(ans,dp[i][j]);
+                }                
+            }
         }
-        lcs(nums1,nums2,0,0,dp);
-        return sln;
+        return ans;
     }
 }
