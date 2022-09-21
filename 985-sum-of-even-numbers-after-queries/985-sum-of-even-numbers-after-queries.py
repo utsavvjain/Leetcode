b042e2b27d1669549d892aedf915ea64f3b39660
utsav jain <74541660+utsavvjain@users.noreply.github.com>
2022-09-21 12:02:18 +0530

    Time: 1139 ms (23.21%), Space: 21 MB (7.14%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
@@ -8,20 +8,12 @@ class Solution:
                 sm+=nums[i]
                 st.add(i)
         for query in queries : 
-            if query[1] not in st : 
-                nums[query[1]]+=query[0]
-                if nums[query[1]]%2==0 : 
-                    st.add(query[1])
-                    sm+=nums[query[1]]
-            else : 
-                b=nums[query[1]]
-                a=nums[query[1]]+query[0]
-                nums[query[1]]=a
-                sm-=b
-                if a%2==0 : 
-                    sm+=a
-                else : 
-                    st.discard(query[1])
+            [val,idx]=query
+            if nums[idx]%2==0 : 
+                sm-=nums[idx]
+            nums[idx]+=val
+            if nums[idx]%2==0 : 
+                sm+=nums[idx]
             ans.append(sm)
         return ans
         
