4601aa374bafb68a811f880ce54c06b932352481
utsav jain <74541660+utsavvjain@users.noreply.github.com>
2022-09-26 12:33:18 +0530

    Time: 3 ms (33.64%), Space: 43.1 MB (18.32%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -5,16 +5,15 @@ class Solution {
         if(a==b) return;
         char parA=find(a);
         char parB=find(b);
-        set[parA]=parB;        
+        set[parA-'a']=parB;        
     }
     public char find(char a){
-        if(a==set[a]) return a;
-        return set[a]=find(set[a]);
+        if(a==set[a-'a']) return a;
+        return set[a-'a']=find(set[a-'a']);
     }
     public boolean equationsPossible(String[] equations) {
-        set=new char[256];
-        for(char i=0;i<256;i++) set[i]=i;
-        List<String> equals=new LinkedList<>();
+        set=new char[26];
+        for(char i='a';i<='z';i++) set[i-'a']=i;
         List<String> notEquals=new LinkedList<>();
         for(String eq : equations){
             if(eq.contains("!=")) notEquals.add(eq);
