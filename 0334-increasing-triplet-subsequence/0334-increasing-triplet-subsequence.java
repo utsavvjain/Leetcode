7006ed4a26618c5d51b026057d2ed4336617443c
utsav jain <74541660+utsavvjain@users.noreply.github.com>
2022-10-11 13:34:55 +0530

    Time: 25 ms (6.06%), Space: 84.3 MB (85.97%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -2,13 +2,16 @@ class Solution {
     public boolean increasingTriplet(int[] nums) {
         List<Integer> list=new ArrayList<>();
         for(int num : nums){
-            if(list.isEmpty() || num>list.get(list.size()-1)) list.add(num);
+            if(list.isEmpty() || num>list.get(list.size()-1)){
+              list.add(num);
+                if(list.size()>2) return true;
+            } 
             else{
                 int position=Collections.binarySearch(list,num);
                 if(position<0) position=(-position)-1;
                 list.set(position,num);
             }
         }
-        return list.size()>=3;
+        return false;
     }
 }
