692f4085a65d43b0ee37411c91dc323ca04a7b4c
utsav jain <74541660+utsavvjain@users.noreply.github.com>
2022-10-28 11:58:59 +0530

    Time: 236 ms (39.81%), Space: 17.2 MB (80.07%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,6 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mapOfWords=collections.defaultdict(list)
+        for word in strs : 
+            mapOfWords[''.join(sorted(word))].append(word)
+        return [value for value in mapOfWords.values()]
